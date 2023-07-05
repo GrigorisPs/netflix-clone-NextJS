4caa7a0ada6b7eb1afb1ca32d49c43085607fb5b
GrigorisPs <73849201+GrigorisPs@users.noreply.github.com>
2023-07-05 22:17:40 +0300

    stripe faction refactor

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,36 +1,38 @@
 import {
     createCheckoutSession,
     getStripePayments,
-} from '@stripe/firestore-stripe-payments'
-import { getFunctions, httpsCallable } from '@firebase/functions'
-import app from '../firebase'
-
-
-
-const payments = getStripePayments(app, {
+  } from '@stripe/firestore-stripe-payments'
+  import { getFunctions, httpsCallable } from '@firebase/functions'
+  import app from '../firebase'
+  
+  const payments = getStripePayments(app, {
     productsCollection: 'products',
-    customersCollection: 'customers'
-})
-
-const loadCheckout = async (priceId: string) => {
+    customersCollection: 'customers',
+  })
+  
+  const loadCheckout = async (priceId: string) => {
     await createCheckoutSession(payments, {
-        price: priceId,
-        success_url: window.location.origin,
-        cancel_url: window.location.origin,
+      price: priceId,
+      success_url: window.location.origin,
+      cancel_url: window.location.origin,
     })
-        .then((snapshot) => window.location.assign(snapshot.url))
-        .catch((error) => console.log(error.message))
-}
-
-const goToBillingPortal = async () => {
+      .then((snapshot) => window.location.assign(snapshot.url))
+      .catch((error) => console.log(error.message))
+  }
+  
+  const goToBillingPortal = async () => {
     const instance = getFunctions(app, 'us-central1')
-    const functionRef = httpsCallable(instance,
-        'ext-firestore-stripe-payments-createPortalLink'
+    const functionRef = httpsCallable(
+      instance,
+      'ext-firestore-stripe-payments-createPortalLink'
     )
+  
     await functionRef({
-        returnUrl: `${window.location.origin}/account`
-    }).then(({ data }: any) => window.location.assign(data.url)
-    ).catch((error) => console.log(error.message))
-}
-export { loadCheckout, goToBillingPortal }
-export default payments
+      returnUrl: `${window.location.origin}/account`,
+    })
+      .then(({ data }: any) => window.location.assign(data.url))
+      .catch((error) => console.log(error.message))
+  }
+  
+  export { loadCheckout, goToBillingPortal }
+  export default payments
